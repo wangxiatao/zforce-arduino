475339ee3b3efd1dc61154c0d0e3b92ef7345b99
tao <wangxiatao@gmail.com>
2020-04-21 14:22:51 +0200

    Add a lot of comments

--- Zforce/example/zForceAirKeyboardMouse/zForceAirKeyboardMouse.ino
+++ Zforce/example/zForceAirKeyboardMouse/zForceAirKeyboardMouse.ino
@@ -3,11 +3,12 @@
 #include <Zforce.h>
 #include <Wire.h>
 
-//#define WAIT_SERIAL_PORT_CONNECTION
-#define TOUCH_BUFFER_SIZE 1
-const int holdTime = 80;
-const int keyboardBoundary = 1100;
-long globalMillis = millis();
+//#define WAIT_SERIAL_PORT_CONNECTION   // uncomment to enforce waiting for serial port connection
+                                        // (not only USB), requires active serial terminal/app to work
+#define TOUCH_BUFFER_SIZE 1             // used by system to buffer touch, "1" means single touch buffer
+const int holdTime = 80;                // sensitivity for mouse "left click", unit in milli-second
+const int keyboardBoundary = 1100;      // separate mouse area and keyboard area on the x-axis
+long globalMillis = millis();           // global timestamp
 
 typedef struct Location
 {
@@ -21,38 +22,49 @@ typedef struct TouchPoint
     uint8_t state;
 } TouchPoint;
 
-Zforce zf = Zforce();
+Zforce zf = Zforce();                   // instatiate touch instance
 uint8_t nTouches = 0;
 TouchPoint tp;
 Location previousLoc;
 Location initialLoc;
-int previousButtonState = HIGH; // for checking the state of a pushButton
-volatile bool newTouchDataFlag = false;
+int previousButtonState = HIGH;         // for checking the state of a pushButton
+volatile bool newTouchDataFlag = false; // new data flag works with data ready pin ISR
 
+/*
+ * Interrupt service callback for Data ready pin, set flag and get data in process()
+ */
 void dataReadyISR() { newTouchDataFlag = true; }
 
+/*
+ * Check if data is ready to fetch
+ * @return true if there is data waiting to fetch
+ */
 bool isDataReady() { return digitalRead(PIN_NN_DR) == HIGH; }
 
+/*
+ * Sensor initialization and configuration
+ * @return true if all steps are excuted successfully
+ */
 bool touchInit()
 {
     globalMillis = millis();
-    pinMode(PIN_NN_DR, INPUT_PULLDOWN);
-    pinMode(PIN_NN_RST, OUTPUT);
+    pinMode(PIN_NN_DR, INPUT_PULLDOWN);     // setup DataReady pin
+    pinMode(PIN_NN_RST, OUTPUT);            // setup Reset pin
     digitalWrite(PIN_NN_RST, LOW);
     delay(10);
-    digitalWrite(PIN_NN_RST, HIGH);
+    digitalWrite(PIN_NN_RST, HIGH);         // Reset sensor
     
     Wire.begin();
     
     uint16_t timeout = 500;
-    while (!isDataReady() && timeout--)
+    while (!isDataReady() && timeout--)     // Wait for BootComplete message to come
         delay(1);
     if(timeout == 0)
     {
         Serial.println("Timeout during getting boot complete");
         return false;
     }
-    Message *msg = zf.GetMessage(); // Get bootcomplete message
+    Message *msg = zf.GetMessage();         // Get bootcomplete message
     if (msg->type == MessageType::BOOTCOMPLETETYPE)
         Serial.println(F("Sensor connected"));
     else
@@ -67,8 +79,8 @@ bool touchInit()
     }
     zf.DestroyMessage(msg);
 
-    zf.Enable(true); // Send and read Enable
-    timeout = 500;
+    zf.Enable(true);                        // Send and get Enable message
+    timeout = 500;                          // Timeout is set to 500ms
     while (!isDataReady() && timeout--)
         delay(1);
     if(timeout == 0)
@@ -83,7 +95,10 @@ bool touchInit()
         return false;
     }
     zf.DestroyMessage(msg);
-    newTouchDataFlag = false;   // clear flag
+
+    newTouchDataFlag = false;               // clear new data flag
+
+    // enable interrupt
     attachInterrupt(digitalPinToInterrupt(PIN_NN_DR), dataReadyISR, RISING);
 
     Serial.print(F("Initialization took "));
@@ -92,12 +107,15 @@ bool touchInit()
     return true;
 }
 
+/*
+ * Mouse event service to convert touch event to mouse event
+ */
 void loopMouse()
 {
-    if(tp.loc.x > keyboardBoundary)
+    if(tp.loc.x > keyboardBoundary)     // outside mouse area, do nothing
         return;
 
-    if (tp.state == 0)
+    if (tp.state == 0)                  // touch "down", buffer the location
     {
         previousLoc.x = initialLoc.x = tp.loc.x;
         previousLoc.y = initialLoc.y = tp.loc.y;
@@ -105,34 +123,40 @@ void loopMouse()
     }
     else
     {
-        if (tp.state == 1)
+        if (tp.state == 1)              // touch "move"
         {
-            if (tp.state == 1 && tp.loc.x <= keyboardBoundary)
+            if (tp.state == 1 && tp.loc.x <= keyboardBoundary)  // within mouse area
             {
-                if((millis() - globalMillis) >= holdTime)
+                if((millis() - globalMillis) >= holdTime)   // not a "left click"
                     Mouse.move((tp.loc.x - previousLoc.x) / 1, (tp.loc.y - previousLoc.y) / 1);
                 previousLoc.x = tp.loc.x;
                 previousLoc.y = tp.loc.y;
             }
         }
-        else if (tp.state == 2)
+        else if (tp.state == 2)         // touch "up"
         {
-            if(millis() - globalMillis < holdTime)
+            if(millis() - globalMillis < holdTime)  // mouse "left click", sensitivity 
+                                                    // can be tuned by changing "holdTime"
             {
                 Mouse.click(MOUSE_LEFT);
             }
-            tp.state = 0xFF;
+            tp.state = 0xFF;            // reset state machine
         }
     }
 }
 
+/*
+ * Keyboard event service to convert touch event to keybard event
+ */
 void loopKeyboard()
 {
-    int buttonState = tp.state < 2; // read the pushbutton:
+    int buttonState = tp.state < 2;     // touch "down" or "move" represents keyboard state LOW
+                                        // and touch "up" represents keyboard state HIGH
 
-    if ((buttonState != previousButtonState) && (buttonState == HIGH))
-    {   // if the button state has changed, and it's currently pressed
-        if (tp.loc.x > keyboardBoundary)
+    if ((buttonState != previousButtonState) && (buttonState == HIGH))  // if the button state has changed, 
+                                                                        // and it's currently pressed
+    {   
+        if (tp.loc.x > keyboardBoundary) // below conditions can be changed to fit your purpose
         {
             if (tp.loc.y < 250)
                 Keyboard.print('A');
@@ -153,6 +177,10 @@ void loopKeyboard()
     previousButtonState = buttonState;  // save the current button state for comparison next time
 }
 
+/*
+ * Get touch events
+ * @return 0 if no touch event is present, -1 if message is not touch message, otherwise the number of touches received
+ */
 uint8_t updateTouch()
 {
     if (newTouchDataFlag == false)
@@ -184,10 +212,12 @@ void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
-    Serial.begin(115200);
+    Serial.begin(115200);       // start serial port, this is optional and is 
+                                // NOT required to run mouse and keybaord
+                                // services, comment out to skip
     
     #ifdef WAIT_SERIAL_PORT_CONNECTION
-    while (!Serial) // 10Hz blink to indicate USB connection is waiting to establish
+    while (!Serial)             // 10Hz blink to indicate USB connection is waiting to establish
     {
         digitalWrite(LED_BUILTIN, LOW);
         delay(50);
@@ -197,15 +227,15 @@ void setup()
     digitalWrite(LED_BUILTIN, HIGH);
     #endif
     
-    Keyboard.begin(); // initialize control over the keyboard
-    Mouse.begin();
+    Keyboard.begin();           // initialize keyboard
+    Mouse.begin();              // initialize mouse
     if(!touchInit())
         Serial.println(F("Touch init failed"));
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_BUILTIN, LOW); // blink when new touch event is received
     updateTouch();
     digitalWrite(LED_BUILTIN, HIGH);
     loopKeyboard();
